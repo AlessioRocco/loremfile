737d3e3d15279ed334e99a5ff90a6a001c574fd6
Andrea D'Ippolito <adedip@gmail.com>
2012-07-05 16:13:47 +0200

    first feature

---
+++ spec/acceptance/steps/lorem_file_steps.rb
@@ -0,0 +1,9 @@
+module LoremFileSteps
+  step "I go to ':url'" do |url|
+    visit url
+  end
+  
+  step "I should get a file named ':file_name' and of ':ext' type" do |file_name, ext|
+    
+  end
+end
