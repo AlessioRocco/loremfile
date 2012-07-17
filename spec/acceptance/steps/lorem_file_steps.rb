a473c5b5bd2aecb8412cad7ddb8a73eeca80483c
Andrea D'Ippolito <adedip@gmail.com>
2012-07-17 15:07:44 +0200

    fix step download

--- spec/acceptance/steps/lorem_file_steps.rb
+++ spec/acceptance/steps/lorem_file_steps.rb
@@ -4,10 +4,7 @@ module LoremFileSteps
   end
   
   step "I should get a file named :file_name and of :ext type" do |file_name, ext|
-    result = page.response_headers['Content-Type'].should == "application/octet-stream"
-    if result
-      result = page.response_headers['Content-Disposition'].should =~ /#{file_name}.#{ext}/
-    end
-    result
+    page.response_headers['Content-Type'].should == "application/octet-stream"
+    page.response_headers['Content-Disposition'].should =~ /#{file_name}.#{ext}/
   end
 end
